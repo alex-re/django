5ba8a8870cdf0b3ff0b3282a9a13f9ba3986f15b
alex-from-ubuntu <chalghoos1@gmail.com>
2020-08-21 17:29:19 +0430

    a bit better front end & bootstrap

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse  # if we have external html files we can comment this importing
 
 # Create your views here.
 
 
+posts = [
+    {
+        'author':'khodam',
+        'title':'Blog post 1',
+        'content':'First post content',
+        'date_posted':'Agust 27, 2018'
+    },
+    {
+        'author':'chalghoos',
+        'title':'Blog post 2',
+        'content':'Second post content',
+        'date_posted':'Agust 30, 2020'
+    }
+]
+
+
 def home(request):
-    return HttpResponse("<h1> Blog Home </h1>")
+    context = {
+        'posts':posts
+    }
+
+    # return HttpResponse("<h1> Blog Home </h1>")
+    return render(request, 'blog/home.html', context)  # ~/django/django_project/blog/templates/blog/home.html
 
 
 def about(request):
-    return HttpResponse("<h1> Blog About </h1>")
+    # return HttpResponse("<h1> Blog About </h1>")
+    return render(request, 'blog/about.html', {'title':'About'})
 
