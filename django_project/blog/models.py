d2e05301a725c55498d53eebc6d92a0415bb8fe2
alex-from-ubuntu <chalghoos1@gmail.com>
2020-08-22 14:48:29 +0430

    working with db

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+from django.utils import timezone
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)  # `auto_now=True` means update time after each update but `auto_now_add=True` means time tht its added.
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # `on_delete` means what we whant to do if user deleted (in this case we delete posts if user deleted but if the post deleted user will not delete!) (if we want to delete post or keep it with none author or ...)
+
+    def __str__(self):
+        return self.title
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,28 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse  # if we have external html files we can comment this importing
+from django.http import HttpResponse  # if we have external html files we can comment this importing.
+
+from .models import Post  # "." means models in this directory.
 
 # Create your views here.
 
 
-posts = [
-    {
-        'author':'khodam',
-        'title':'Blog post 1',
-        'content':'First post content',
-        'date_posted':'Agust 27, 2018'
-    },
-    {
-        'author':'chalghoos',
-        'title':'Blog post 2',
-        'content':'Second post content',
-        'date_posted':'Agust 30, 2020'
-    }
-]
+# posts = [
+#     {
+#         'author':'khodam',
+#         'title':'Blog post 1',
+#         'content':'First post content',
+#         'date_posted':'Agust 27, 2018'
+#     },
+#     {
+#         'author':'chalghoos',
+#         'title':'Blog post 2',
+#         'content':'Second post content',
+#         'date_posted':'Agust 30, 2020'
+#     }
+# ]
 
 
 def home(request):
     context = {
-        'posts':posts
+        # 'posts':posts
+        'posts':Post.objects.all()
     }
 
     # return HttpResponse("<h1> Blog Home </h1>")
