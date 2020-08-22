dfc7a64b6d0a085aff9d1792f16df4633e47d1fd
alex-from-ubuntu <chalghoos1@gmail.com>
2020-08-22 16:18:05 +0430

    try to loading gif but cant NOW ;)

--- django_project/blog/static/blog/base.js
+++ django_project/blog/static/blog/base.js
@@ -5,4 +5,10 @@ $(document).ready ( function(){
 
     div_navbar_bug.style.height = navbar.offsetHeight+'px';
     div_navbar_bug.style.width = navbar.offsetWidth+'px';
+    
+    
+    // ________________________LOADING_GIF____________________________ 
+    // $("#loading").hide(10000);
+    // $("#loading").fadeOut("3000");
+    // $("#loading").fadeOut("3000");
 });
