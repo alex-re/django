b294cca7735a00046a8c0f997a152d1fba8b5e3d
alex-from-ubuntu <chalghoos1@gmail.com>
2020-08-23 20:23:02 +0430

    not bad loading

--- django_project/blog/static/blog/base.js
+++ django_project/blog/static/blog/base.js
@@ -1,14 +1,12 @@
 $(document).ready ( function(){
+    // $("#loading").fadeOut(1000);
+    $("#loading").hide("slow");
+    
     var navbar, div_navbar_bug;
     navbar = document.getElementById('top_navbar');
     div_navbar_bug = document.getElementById("navbar_bug");
-
+    
     div_navbar_bug.style.height = navbar.offsetHeight+'px';
     div_navbar_bug.style.width = navbar.offsetWidth+'px';
-    
-    
-    // ________________________LOADING_GIF____________________________ 
-    // $("#loading").hide(10000);
-    // $("#loading").fadeOut("3000");
-    // $("#loading").fadeOut("3000");
+
 });
