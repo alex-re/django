89d361ca023584814e4ad866b2c109a7c00c654f
alex-from-ubuntu <chalghoos1@gmail.com>
2020-08-24 14:11:12 +0430

    registeration

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from users import views as user_views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'),
     path('', include('blog.urls')),  # blog/ | bloog_dev/ (I think its better no NOT leave it emty for privention of possible conflicts!)
     
 ]
