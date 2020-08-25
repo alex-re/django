c15932d5a29cf6012b6f49938d035aa44cde2769
alex-from-ubuntu <chalghoos1@gmail.com>
2020-08-25 15:45:11 +0430

    defalt png

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 
 from users import views as user_views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -28,6 +30,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),  # `as_view` is for class views.
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls')),  # blog/ | bloog_dev/ (I think its better no NOT leave it emty for privention of possible conflicts!)
-    
+
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
