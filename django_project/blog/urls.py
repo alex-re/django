8bfe1e52aafc280dc9457bee1926e4c4fcaff1e8
alex-from-ubuntu <chalghoos1@gmail.com>
2020-08-27 01:20:54 +0430

    update and delete posts

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
 from . import views
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
+
 
 urlpatterns = [
-    path('', views.home, name="blog-home"),
+    # path('', views.home, name="blog-home"),
+    path('', PostListView.as_view(), name="blog-home"),  # if .as_view (emty) it will look for <app>/<model>_<veiwtype>.html in this case -> blog/post_list.html
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  # integer of primery key
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name="blog-about"),
-
-
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse  # if we have external html files we can comment this importing.
 
 from .models import Post  # "." means models in this directory.
+from django.views.generic import (  # ListView is class based view.
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
 
@@ -31,6 +39,51 @@ def home(request):
     # return HttpResponse("<h1> Blog Home </h1>")
     return render(request, 'blog/home.html', context)  # ~/django/django_project/blog/templates/blog/home.html
 
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']  # "-" for reverse it (newer in top)
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):  # those has to be on the left
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def about(request):
     # return HttpResponse("<h1> Blog About </h1>")
