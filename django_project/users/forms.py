ec7abde8f4d8bb62fe09e4218a7d487ac648944b
alex-from-ubuntu <chalghoos1@gmail.com>
2020-08-26 13:12:31 +0430

    .

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -9,3 +10,15 @@ class UserRegisterForm(UserCreationForm):
     class Meta:  # its about cofigurations for example now we are saying that its a "User model" and it has these fields.
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    pass
