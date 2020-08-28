f2a87e630567c29247b4b5122f18620b366d68ae
alex-from-ubuntu <chalghoos1@gmail.com>
2020-08-28 13:45:07 +0430

    password reset

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -29,6 +29,20 @@ urlpatterns = [
     # path('login/', user_views.login, name='login'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),  # `as_view` is for class views.
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    
+    path('password-reset/',
+        auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),
+        name='password_reset'),
+    path('password-reset/done/',
+        auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+        name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',  # uidb64: -> user id in base 64.
+        auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+        name='password_reset_confirm'),
+    path('password-reset-complete/',
+        auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+        name='password_reset_complete'),
+
     path('', include('blog.urls')),  # blog/ | bloog_dev/ (I think its better no NOT leave it emty for privention of possible conflicts!)
 
 ]
